277c9b9cff4cdfbbeb5000010fba94e94bb2d257
ArtiomP <artiom.penschii91@gmail.com>
2022-05-17 14:37:55 +0300

    Исправил задание 2 в ДЗ и добавил вариент через List (Tuple)

---
+++ Csharp_study/03.Csharp_homework/03.Penschii_Artiom_Homework/Task2.2_List_method/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+
+Console.WriteLine("Задача 21:\n Напишите программу, которая принимает на вход координаты двух точек\n и находит расстояние между ними в 3D пространстве.");
+
+/* Способ решения через список*/
+
+/* Метод проверки ввода */
+
+int EnterNumberTest(){
+    bool readlineFromStrToInt = true;
+    int number =0;
+    while (readlineFromStrToInt){
+        string numberSTR = Console.ReadLine();
+        if (int.TryParse(numberSTR, out int numberInt) ){
+            number = numberInt;
+            readlineFromStrToInt = false;
+        }
+        else Console.Write("Вы ввели текст, введите пожалуйста число : "); 
+    }
+    return number;
+}
+
+/* Метод создания списка. Пока что на 2 точки, но можно легко исправить на N точке лишь добавив в метод N и в for i<n*/
+List<(int,int,int)> coordEnter(){
+    List<(int,int,int)> coodrinates = new List<(int,int,int)>();
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    for (int i = 0; i< 2; i++){
+        Console.Write($"Введите точку {i+1} X :");
+        x = EnterNumberTest();
+        Console.Write($"Введите точку {i+1} Y :");
+        y = EnterNumberTest();
+        Console.Write($"Введите точку {i+1} Z :");
+        z = EnterNumberTest();
+        coodrinates.Add((x,y,z));
+        Console.WriteLine();
+    }
+return coodrinates;
+}
+
+/* Метод рассчёта длины по 2м точкам в 3d*/
+
+double lengthCount(List<(int,int,int)> coodrinates){
+    double d = (double)Math.Sqrt(Math.Pow(coodrinates[0].Item1-coodrinates[1].Item1 , 2)+Math.Pow(coodrinates[0].Item2-coodrinates[1].Item2 , 2)+Math.Pow(coodrinates[0].Item3-coodrinates[1].Item3 , 2));
+return d;
+}
+  
+/* Сама программа */
+List<(int,int,int)> coodrinates = coordEnter();
+
+Console.WriteLine($"Length between Coord1{coodrinates[0]} & Coord2{coodrinates[1]} = {lengthCount(coodrinates):N2}") ;
+
+Console.ReadLine();
--- Csharp_study/03.Csharp_homework/03.Penschii_Artiom_Homework/Task2/Program.cs
+++ Csharp_study/03.Csharp_homework/03.Penschii_Artiom_Homework/Task2/Program.cs
@@ -2,6 +2,9 @@
 
 Console.WriteLine("Задача 21:\n Напишите программу, которая принимает на вход координаты двух точек\n и находит расстояние между ними в 3D пространстве.");
 
+/* Способ решения через массив*/
+
+/* Метод проверки ввода*/
 int EnterNumberTest(){
     bool readlineFromStrToInt = true;
     int number =0;
@@ -17,34 +20,44 @@ int EnterNumberTest(){
     return number;
 }
 
-void Test(int i, int j){
-    if ( (i == 0) && (j == 0)) Console.Write(" Enter X1 : ");
-    else if ( (i == 0) && (j == 1)) Console.Write(" Enter Y1 : ");
-    else if ( (i == 0) && (j == 2)) Console.Write(" Enter Z1 : ");
-    else if ( (i == 1) && (j == 0)) Console.Write(" Enter X2 : ");
-    else if ( (i == 1) && (j == 1)) Console.Write(" Enter Y2 : ");
-    else  Console.Write(" Enter Z2 : "); 
+/* Метод указания какая координата вводится*/
+void writeWhatCoord(int i, int j){
+    if ((j == 0)) Console.Write($"\t X{i+1} : ");
+    else if ((j == 1)) Console.Write($"\t Y{i+1} : ");
+    else  Console.Write($"\t Z{i+1} : ");
 }
 
+/* Метод ввода кординат*/
 int[,] CoordEnter(){
     int[,] array = new int[2,3];
     for (int i = 0; i < 2; i++){
+        Console.WriteLine($"Coodinate {i+1} : ");
         for (int j = 0; j < 3; j++){
-            Test(i,j);
+            writeWhatCoord(i,j);
             array[i,j] = EnterNumberTest();
         }
     }
 return array;
 }
+/* Метод ввода кординат, универсальный
+int[,] CoordEnter(int coordCount, int ploskosti){
+    int[,] array = new int[coordCount,ploskosti];
+    for (int i = 0; i < coordCount; i++){
+        for (int j = 0; j < ploskosti; j++){
+            writeWhatCoord(i,j);
+            array[i,j] = EnterNumberTest();
+        }
+    }
+return array;
+}*/
 
+/* Метод рассчёта длины отрезка через 2 точки в 3д плоскости*/
 double lengtCount(int[,] array){
     double d = (double)Math.Sqrt(Math.Pow(array[1,0]-array[0,0] , 2)+Math.Pow(array[1,1]-array[0,1] , 2)+Math.Pow(array[1,2]-array[0,2] , 2));
 return d;
 }
-
-int[,] array = new int[2,2];
-
-array = CoordEnter();
+/* Тело программы*/
+int[,] array = CoordEnter();
 
 Console.WriteLine($"Length = {lengtCount(array):N2}");
 
