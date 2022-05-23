d4b1013cacd3e1c31df2c22db607f699629f0c25
artiom <artiom.penschii91@gmail.com>
2022-05-23 21:16:37 +0300

    Seminar

---
+++ Csharp_study/02.Csharp_seminar/05.Seminar/Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+
+Console.Clear();
+
+int[] arrayCreation(int length){
+    int[] arr = new int[length];
+    Random rnd = new Random();
+    for ( int i = 0 ; i< length ; i ++){
+        arr[i] = rnd.Next(-9,10);
+    }
+return arr;}
+
+int getMinSumArray(int[] array){
+    int sum = 0;
+    foreach (int item in array){
+        if (item<0)
+            sum += item;
+    }
+return sum;}
+
+int geMaxSumArray(int[] array){
+    int sum = 0;
+    foreach (int item in array){
+        if (item>0)
+            sum += item;
+    }
+return sum;}
+
+void printArray(int[] array){
+    foreach(int item in array){
+        Console.Write($"{item} ");
+    }
+}
+
+(int , int ) GetSumFormArray(int[] array){
+    int sumMun = 0;
+    int sumMax = 0;
+    foreach ( int item in array){
+        if (item>0)
+            sumMax += item;
+        else sumMun += item;
+    }
+
+return (sumMun,sumMax);}
+
+int[] array = arrayCreation(12);
+printArray(array);
+
+int sum1 = getMinSumArray(array);
+int sum2 = geMaxSumArray(array);
+
+Console.WriteLine();
+Console.WriteLine($"sum < 0 = {sum1} and sum > 0 {sum2}");
+
+(sum1 , sum2) = GetSumFormArray(array);
+Console.WriteLine($"sum < 0 = {sum1} and sum > 0 {sum2}");
+
---
+++ Csharp_study/02.Csharp_seminar/05.Seminar/Task5/Program.cs
@@ -0,0 +1,45 @@
+﻿//Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+Console.Clear();
+
+int[] arrayCreation(int length){
+    int[] arr = new int[length];
+    Random rnd = new Random();
+    for ( int i = 0 ; i< length ; i ++){
+        arr[i] = rnd.Next(0,51);
+    }
+return arr;}
+
+string PrintArray(int[] array){
+    string result = String.Empty;
+    for (int i = 0; i < array.Length; i++){
+        if (i == 0) result = "[ "+result + Convert.ToString(array[i]) + ", ";
+        else if (i < array.Length-1)
+            result = result + Convert.ToString(array[i]) + ", ";
+        else result = result + Convert.ToString(array[i]) + "]";
+    }
+return result;
+}
+
+string PrintList(List<int> list){
+    string result = String.Empty;
+    for (int i = 0; i < array.Length; i++){
+        if (i == 0) result = "[ "+result + Convert.ToString(array[i]) + ", ";
+        else if (i < array.Length-1)
+            result = result + Convert.ToString(array[i]) + ", ";
+        else result = result + Convert.ToString(array[i]) + "]";
+    }
+return result;
+}
+
+List<int> sumNumbers(int[] array){
+    List<int> list= new List<int>();
+    for (int i = 0; i < array.Length/2 ; i++){
+        list.Add(array[i]*array[array.Length-1-i]);
+    }
+return list;
+}
+
+int[] array = arrayCreation(10);
