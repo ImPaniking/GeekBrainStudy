5c4ba9ca529748ad4268d0b7b43d726f7a8bf0be
artiom <artiom.penschii91@gmail.com>
2022-05-30 06:55:31 +0300

    HomeWork 6

---
+++ Csharp_study/03.Csharp_homework/06.Penschii_Artiom_Homework/Task1_easy/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.Clear();
+
+int numbersInput(int numberCount)//Метод ввода цифр по очереди
+{
+    Console.WriteLine("Вводите числа через ENTER :");
+    int count = 1;
+    int countNegative = 0;
+    int number = 0;
+    while (count <= numberCount)
+    {
+        Console.Write($"({count}) : ");
+        number = int.Parse(Console.ReadLine());
+        count++;
+        if ( number < 0)
+            countNegative++;
+    }
+return countNegative;
+}
+
+int numbersRandom(int numberCount)//Метод случайного генерирования цифр ( ну что бы не парится с вводом )
+{
+    Random rnd = new Random();
+    int count = 1;
+    int countNegative = 0;
+    int number = 0;
+    while (count <= numberCount)
+    {
+        number = rnd.Next(-10,11);
+        Console.Write($"{number} ");
+        count++;
+        if ( number < 0)
+            countNegative++;
+    }
+return countNegative;
+}
+
+
+Console.Write("Введите количество чисел M :");
+int M = int.Parse(Console.ReadLine());
+
+/*Console.Write($" Отрицательных чисел - > {numbersInput(M)}");*/ // Тут при записи вводятся цифры и сразу выдаётся ответ 
+int count = numbersRandom(M);
+Console.Write($" - > {count}");
---
+++ Csharp_study/03.Csharp_homework/06.Penschii_Artiom_Homework/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.Clear();
+
+int b1 = 2;
+int k1 = 5;
+int b2 = 4;
+int k2 = 9;
+
+float y=0;
+float x=0;
+
+if (b1 > b2 && k1 > k2)
+    x = (float)(b1 - b2)/(k1 - k2);
+if (b1 < b2 && k1 < k2)
+    x = (float)(b2 - b1)/(k2 - k1);
+if (b1 > b2 && k1 < k2)
+    x = (float)(b1 - b2)/(k2 - k1);
+if (b1 < b2 && k1 > k2)
+    x = (float)(b2 - b1)/(k1 - k2);
+
+Console.Write($"{x:N2}");
+
+if ( k1 > k2)
+    y = k2 * x + b2;
+else
+    y = k1 * x + b1;
