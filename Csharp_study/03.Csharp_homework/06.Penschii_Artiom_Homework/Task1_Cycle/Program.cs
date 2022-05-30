9a4fe2d5402b9304f7944d7ae42239bb6f93d105
ArtiomP <artiom.penschii91@gmail.com>
2022-05-30 10:23:30 +0300

    Доделал домашнее задание 6

---
+++ Csharp_study/03.Csharp_homework/06.Penschii_Artiom_Homework/Task1_Cycle/Program.cs
@@ -0,0 +1,60 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.Clear();
+
+int NumberInput(string inputText)//Метод ввода и проверки на число
+{
+    bool isInputStringInt = true;
+    int number =0;
+    while (isInputStringInt)
+    {
+        Console.Write($"{inputText} :");
+        string numberSTR = Console.ReadLine();
+        if (int.TryParse(numberSTR, out int numberInt))
+        {
+            number = numberInt;
+            isInputStringInt = false;
+        }
+        else 
+            Console.Write("Ввели не число ");
+    }
+    return number;
+}
+
+(int[] , int) NumberArrayInput(int arrayLength)//Метод задания массива из кол-ва чисел M, решил сразу вывести и количество негативных
+{
+    int[] array = new int[arrayLength];
+    int negativeNumbersCount = 0;
+    for (int i = 0 ; i < arrayLength ; i ++)
+    {
+        array[i] = NumberInput(inputText: $"{i+1}");
+        if (array[i] < 0)
+            negativeNumbersCount++;
+    }    
+    return (array,negativeNumbersCount);
+}
+
+void PrintArray(int[] array)//метод печати массива как в примере - чисто для красоты
+{
+    for ( int i = 0 ; i < array.Length ; i ++)
+        {
+            Console.Write(array[i]);
+            if ( i < array.Length - 1)
+                Console.Write(", ");
+        }
+}
+
+Console.Write(
+    "Пользователь вводит с клавиатуры M чисел."+
+    "\nПосчитайте, сколько чисел больше 0 ввёл пользователь."+
+    "\n\nПример : 0, 7, 8, -2, -2 -> 2");
+
+int numbersAmmount = NumberInput("\n\nКоличество чисел M");//Вводится с клавиатуры какое-количество чисел будет
+Console.WriteLine();
+(int[] numbersArray , int negativeNumbersCount) = NumberArrayInput(numbersAmmount);//Создаётся массив чисел и даётся количество отрицательных
+Console.WriteLine();
+PrintArray(numbersArray);//Оформленрие результата
+Console.Write($" - > {negativeNumbersCount}");
--- Csharp_study/03.Csharp_homework/06.Penschii_Artiom_Homework/Task2/Program.cs
+++ Csharp_study/03.Csharp_homework/06.Penschii_Artiom_Homework/Task2/Program.cs
@@ -1,29 +1,69 @@
 ﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
 
+//https://profmeter.com.ua/communication/learning/course/course19/lesson194/ - взял теорию отсюда
+//k- угловой коефициент
+//b - сводный член в уравнении прямой
+// k1 - не должно ровнятся k2
+
 System.Console.OutputEncoding = System.Text.Encoding.Unicode;
 Console.Clear();
 
-int b1 = 2;
-int k1 = 5;
-int b2 = 4;
-int k2 = 9;
+int NumberInput(string text)//Метод ввода и проверки на число
+{
+    bool isInputStringInt = true;
+    int number =0;
+    while (isInputStringInt)
+    {
+        Console.Write($"Введите {text} :");
+        string numberSTR = Console.ReadLine();
+        if (int.TryParse(numberSTR, out int numberInt))
+        {
+            number = numberInt;
+            isInputStringInt = false;
+        }
+        else 
+            Console.WriteLine("Ввели не число");
+    }
+    return number;
+}
 
-float y=0;
-float x=0;
+(float,float) IntersectionPoint(int b1,int k1,int b2,int k2)//Метод расчёта точки пересечения
+{
+    float x=(float)(b2-b1)/(k1-k2);
+    float y=(float)(k1*x+b1);
+    return(x,y);
+}
 
-if (b1 > b2 && k1 > k2)
-    x = (float)(b1 - b2)/(k1 - k2);
-if (b1 < b2 && k1 < k2)
-    x = (float)(b2 - b1)/(k2 - k1);
-if (b1 > b2 && k1 < k2)
-    x = (float)(b1 - b2)/(k2 - k1);
-if (b1 < b2 && k1 > k2)
-    x = (float)(b2 - b1)/(k1 - k2);
 
-Console.Write($"{x:N2}");
+Console.Write(
+    "Напишите программу, которая найдёт точку пересечения двух прямых,"+
+    "\nзаданных уравнениями y = k1 * x + b1, y = k2 * x + b2;"+
+    "\nзначения b1, k1, b2 и k2 задаются пользователем.\n");
+Console.Write("\nПример: b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)\n\n");
 
-if ( k1 > k2)
-    y = k2 * x + b2;
-else
-    y = k1 * x + b1;
+/*
+//Минимум переменных
+(float x,float y) = IntersectionPoint
+        (
+        b1: NumberInput("b1"),
+        k1: NumberInput("уголовой коэф. k1"),
+        b2: NumberInput("b2"),
+        k2: NumberInput("уголовой коэф. k2")
+        );
+Console.WriteLine($"({x}; {y})");
+*/
+//Более корректное оформление
+
+int b1 = NumberInput("b1");
+int k1 = NumberInput("уголовой коэф. k1");
+int b2 = NumberInput("b2");
+int k2 = NumberInput("уголовой коэф. k2");
+
+if (k1 == k2)
+    Console.WriteLine("Линии не пересекаются");
+else 
+{
+    (float x, float y) = IntersectionPoint(b1,k1,b2,k2);
+    Console.WriteLine($"\nb1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} - > ({x:N1}; {y:N1})");
+}
