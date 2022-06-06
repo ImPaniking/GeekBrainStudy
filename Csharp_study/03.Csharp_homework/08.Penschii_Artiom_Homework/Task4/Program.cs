0d372d61cb73eca78f41e92ca9713bd061b3a405
ArtiomP <artiom.penschii91@gmail.com>
2022-06-06 12:05:27 +0300

    Доделал домашнее задание 8

--- Csharp_study/03.Csharp_homework/08.Penschii_Artiom_Homework/Task4/Program.cs
+++ Csharp_study/03.Csharp_homework/08.Penschii_Artiom_Homework/Task4/Program.cs
@@ -33,17 +33,35 @@ int[,,] ArrayCreation(int xDem, int yDem, int zDem)
     for (int x = 0 ; x < xDem ; x++)
         for (int y = 0 ; y < yDem ; y++)
             for (int z = 0 ; z < zDem ; z++)
-                array[x,y,z] = rnd.Next(10,100);
+            {
+                //Способ ввода случайного числа и проверки - если такое число уже есть , то пробует снова 
+                bool uniqueNumber = true;
+                while (uniqueNumber)
+                {
+                    array[x,y,z] = rnd.Next(10,100);
+                    uniqueNumber = LookUpNumberInArray(array[x,y,z],array);
+                }
+            }
     return array;
 }
 
 void PrintArrayLine(int[,,] array)
 {
-    Console.Write($"\nx\ty\tz\tElement\n");
+    Console.Write($"\nx.y.z.\tNum\n");
     for (int x = 0 ; x < array.GetLength(0) ; x++)
         for (int y = 0 ; y < array.GetLength(1) ; y++)
             for (int z = 0 ; z < array.GetLength(2) ; z++)
-                Console.Write($"{x}\t{y}\t{z}\t{array[x,y,z]}\n");
+                Console.Write($"{x}.{y}.{z}.\t{array[x,y,z]}\n");
+}
+
+bool LookUpNumberInArray(int number, int[,,] array)//Метод проверки есть ли такое число в массиве
+{
+    bool result = true;
+    for ( int x = 0 ; x < array.GetLength(0) ; x++)
+        for ( int y = 0 ; y < array.GetLength(1) ; y++)
+            for ( int z = 0 ; z < array.GetLength(2) ; z++)
+                if ( number == array[x,y,z]) result = false;
+    return result;
 }
 
 Console.Write("Задача 60:"+
@@ -54,5 +72,11 @@ Console.Write("Задача 60:"+
 int xRows = NumberInput("кол-во эллементов по X");
 int yColums = NumberInput("кол-во эллементов по Y");
 int zAmmount = NumberInput("кол-во эллементов по Z");
-int[,,] array = ArrayCreation(xRows,yColums,zAmmount);
-PrintArrayLine(array);
+//В условии прописанно что массив создан из "неповторяющихся двузначных чисел" - то есть от 10 до 99, по этому массив не может быть больше 90 символов. 
+if (xRows*yColums*zAmmount > 90)
+    Console.WriteLine("Количество цифр в массиве не позволяет использовать уникальные двухзначные числа");
+else
+{
+    int[,,] array = ArrayCreation(xRows,yColums,zAmmount);
+    PrintArrayLine(array);
+}
