3d1c21374e1aac2de54660635a99aa2361959486
ArtiomP <artiom.penschii91@gmail.com>
2022-06-09 13:19:09 +0300

    Добавил практические задания

---
+++ Csharp_study/04.Other_lessons(VScode)/Leetcode/LookUpNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] ArrayCreation(int length)
+{
+    Random rnd = new Random();
+    int[] numbersArray = new int[length];
+    for (int i = 0 ; i < length ; i++)
+        numbersArray[i] = rnd.Next(0,10);
+    return numbersArray;
+}
+
+string LookUpIndex(int[] array, int target)
+{
+    string result= "None";
+    for (int i = 0 ; i < array.Length-1 ; i++)
+        for ( int j = i+1 ; j < array.Length; j++)
+            if (array[i]+array[j] == target)
+            {
+                result = $"[{i},{j}]";
+                break;
+            } 
+    return result;   
+}
+
+int target = 6;
+int[] array = ArrayCreation(10);
+for (int i = 0 ; i < array.Length ; i ++)
+    Console.Write($"{array[i]} ");
+Console.WriteLine($"-> {LookUpIndex(array,target)} = {target}");
---
+++ Csharp_study/04.Other_lessons(VScode)/Leetcode/RomeNumberToInt/Program.cs
@@ -0,0 +1,43 @@
+﻿//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M
+
+//Symbol Value
+//I             1
+//V             5
+//X             10
+//L             50
+//C             100
+//D             500
+//M             1000
+
+System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.Clear();
+
+var RomeNumbers = new Dictionary<string, int>()
+{
+    {"I",1},
+    {"V",5},
+    {"X",10},
+    {"L",50},
+    {"C",100},
+    {"D",500},
+    {"M",1000}
+};
+
+Console.Write("Введите число римскими буквами : ");
+string romeNumber = Console.ReadLine();
+int result = 0;
+for ( int i = 0 ; i < romeNumber.Length ; i++)
+{
+    if ( i == romeNumber.Length-1 ) result += RomeNumbers[$"{romeNumber[i]}"];
+    else 
+    {
+        if (RomeNumbers[$"{romeNumber[i]}"]<RomeNumbers[$"{romeNumber[i+1]}"])
+        {
+            result += RomeNumbers[$"{romeNumber[i+1]}"]-RomeNumbers[$"{romeNumber[i]}"];
+            i++;
+        } 
+    else result += RomeNumbers[$"{romeNumber[i]}"];
+    } 
+}
+Console.WriteLine($"{romeNumber} - > {result}");    
+
