fc98905cd4601ebe7c88cd7c8357d2c37d9f9e1d
artiom <artiom.penschii91@gmail.com>
2022-06-15 20:51:22 +0300

    Разное

---
+++ Csharp_study/02.Csharp_seminar/10.Seminar/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 71: В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+
+System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.Clear();
+
+int NumberInput(string text)//Метод ввода и проверки на число
+{
+    bool isInputInt = true;
+    int number =0;
+    while (isInputInt)
+    {
+        Console.Write($"Введите {text} :");
+        string numberSTR = Console.ReadLine();
+        if (int.TryParse(numberSTR, out int numberInt))
+        {
+            if (numberInt <= 0) Console.WriteLine("Введите число больше нуля");
+            else
+            {
+                number = numberInt;
+                isInputInt = false;
+            } 
+        }
+        else 
+            Console.WriteLine("Ввели не число");
+    }
+    return number;
+}
+
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+
+int k = NumberInput("кол-во букв в слове");
+
+FindWords("аисв", new char[k]);
