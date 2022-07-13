d42937ba520d961498a58471746219d089bc2c3b
artiom <artiom.penschii91@gmail.com>
2022-07-13 21:40:10 +0300

    Вторая лекция

--- Pytnon_first_study/test.py
+++ Pytnon_first_study/test.py
@@ -1,7 +1,7 @@
-# with open('test.txt', 'w') as test:
-#     for i in range ( 1 , 6):
-#         test.write(f"{i} - Hello World\n")
-# test.close()
+with open('test.txt', 'w') as test:
+    for i in range ( 1 , 6):
+        test.write(f"{i} - Hello World\n")
+test.close()
 
 test = open('test.txt','r')
 list_of_lines =[]
@@ -12,3 +12,5 @@ for line in test:
 for i in range(0,len(list_of_lines)):
     print(list_of_lines[i])
 test.close()
+
+print(list_of_lines)
