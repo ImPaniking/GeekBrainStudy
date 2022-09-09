44555b6f136a403f3ae626918a65eed14e85d7a8
artiom <artiom.penschii91@gmail.com>
2022-09-09 22:39:05 +0300

    Java

--- Java_first_lection/03.HomeWork/01.HomeWork/Program/Program.java
+++ Java_first_lection/03.HomeWork/01.HomeWork/Program/Program.java
@@ -1,20 +1,41 @@
 public class Program {
     public static void main(String[] args) {
         int a = 5;
-        int b = 5;
+        int b = 35;
         int c = 2;
-        int d = 1;
-        int res = a;
+        int d = 5;
         if ( a > b) System.out.printf("a : %d bigger then b : %d , no point in starting",a,b);
         else if (a == b) System.out.printf("a : %d equal's b : %d , no point in starting",a,b);
-
+        else System.out.printf(workwork(a, b, c, d));
     }
     static boolean TestK1(int a,int b,int c,int d){
-        while ( a*c <= b) a*=c;
+        while ( a*c < b) a*=c;
         return a == b;
     }
     static boolean TestK2(int a,int b,int c,int d){
-        while ( a+d <= b) a+=d;
+        while ( a+d < b) a+=d;
         return a == b;
+    }
+    static String workwork(int a,int b,int c,int d){
+        String result = "";
+        do{
+            if (TestK1(a,b,c,d)){
+                result += "K1";
+                a *= c;
+            } else if (TestK2(a,b,c,d)){
+                result += "K2";
+                a += d;
+            } else {
+                if (a*c <= b) {
+                    result += "K1";
+                    a *= c;
+                } else if (a+d <= b){
+                    result += "K2";
+                    a += d;
+                } else result += "Eror";
+            }
+        } while(a<b);
+        if (result.substring(result.length()-4,result.length()-1) == "Eror") result = "Eror";
+        return result;
     }
 }
